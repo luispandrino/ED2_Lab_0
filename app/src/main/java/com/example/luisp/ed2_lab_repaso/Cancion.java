d1afea1cf2ea4d0697f020f970b70c030692fa16
Luis Andrino <landrinomenendez@gmail.com>
2018-09-04 18:41:44 -0600

    Well the book was obviously wrong.

---
+++ app/src/main/java/com/example/luisp/ed2_lab_repaso/Cancion.java
@@ -0,0 +1,33 @@
+package com.example.luisp.ed2_lab_repaso;
+
+public class Cancion {
+    public String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String Artist;
+
+    public String getArtist() {
+        return Artist;
+    }
+
+    public void setArtist(String artist) {
+        Artist = artist;
+    }
+
+    public String Duration;
+
+    public String getDuration() {
+        return Duration;
+    }
+
+    public void setDuration(String duration) {
+        Duration = duration;
+    }
+}
---
+++ app/src/main/java/com/example/luisp/ed2_lab_repaso/Reproductor.java
@@ -0,0 +1,46 @@
+package com.example.luisp.ed2_lab_repaso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Reproductor extends AppCompatActivity {
+
+    private Button Btn;
+    private EditText Txt;
+    private ListView list;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> Playlist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reproductor);
+        Btn = (Button) findViewById(R.id.btnBuscar);
+        Txt = (EditText)findViewById(R.id.txtBuscar);
+        list = (ListView) findViewById(R.id.lstPlay);
+        Playlist = new ArrayList<String>();
+
+        adapter = new ArrayAdapter<String>(getApplicationContext(),android.R.layout.simple_spinner_item, Playlist);
+        list.setAdapter(adapter);
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Playlist.add(Txt.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+
+    }
+
+}
